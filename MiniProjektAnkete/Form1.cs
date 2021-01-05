aba2c84c38699c241476ce690bb5a3652a776453
Žiga Meža <ziga.meza@scv.si>
2021-01-05 12:58:23 +0100

    Add project files.

---
+++ MiniProjektAnkete/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace MiniProjektAnkete
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string connect = BazaConn.connect();
+
+        public void wtf()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(connect))
+            {
+                con.Open();
+                con.Close();
+            }
+        }
+    }
+}
